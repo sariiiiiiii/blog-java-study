9c0a3ffc2945d5aae6670b81caf1bd8358755ac8
SONGSUNGGEUN <abdf13423@naver.com>
2022-11-22 20:34:55 +0900

    static import

--- src/test/java/com/blog/api/controller/PostControllerTest.java
+++ src/test/java/com/blog/api/controller/PostControllerTest.java
@@ -11,6 +11,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class PostControllerTest {
@@ -22,21 +25,21 @@ class PostControllerTest {
     @DisplayName("get test")
     public void get() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/gets"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("hello"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"))
+                .andDo(print());
     }
 
     @Test
     @DisplayName("post test")
     public void post() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(APPLICATION_JSON)
                         .content("{\"title\": \"\", \"content\": \"콘텐츠 입니다.\"}")
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("타이틀을 입력해주세요"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요"))
+                .andDo(print());
     }
 
 }
