6bccefecbcb0612c37cff4025e458dee1ffce225
sariiiiiiii <abdf13423@naver.com>
2022-11-23 17:05:47 +0900

    blog-java-study

--- src/test/java/com/blog/api/controller/PostControllerTest.java
+++ src/test/java/com/blog/api/controller/PostControllerTest.java
@@ -1,9 +1,15 @@
 package com.blog.api.controller;
 
+import com.blog.api.domain.Post;
+import com.blog.api.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,11 +18,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest
+@AutoConfigureMockMvc
+@SpringBootTest
 class PostControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @BeforeEach
+    public void before() {
+        postRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("get test")
@@ -34,9 +49,41 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"\", \"content\": \"콘텐츠 입니다.\"}")
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("타이틀을 입력해주세요"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.validation.title").value("타이틀을 입력해주세요"))
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("DB에 값 저장")
+    public void post_v2() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/test")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"title\" :  \"타이틀 입니다.\", \"content\" : \"콘텐츠 입니다.\"}")
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("DB에 값 저장")
+    public void test() throws Exception {
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/test")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"타이틀 입니다.\", \"content\": \"콘텐츠 입니다.\"}")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+        // then
+        Assertions.assertEquals(1L, postRepository.count());
+        Post post = postRepository.findAll().get(0);
+        Assertions.assertEquals("타이틀 입니다.", post.getTitle());
+        Assertions.assertEquals("콘텐츠 입니다.", post.getContent());
     }
 
 }
